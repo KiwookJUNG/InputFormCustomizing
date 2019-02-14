d829852744af7c308aec0807a20ae14c3f9a3b8b
정기욱 <rldnr56@me.com>
2019-02-14 16:58:32 +0900

    3개의 레이블을 프로그래밍 방식으로 추가

--- InputFormCustomizing/ViewController.swift
+++ InputFormCustomizing/ViewController.swift
@@ -11,8 +11,35 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 1. 네비게이션 바 타이틀을 입력한다.
+        self.navigationItem.title = "설정"
+        
+        // 2. 이메일 레이블을 생성하고 영역과 기본 문구를 설정한다.
+        let lblEmail = UILabel()
+        lblEmail.frame = CGRect(x: 30, y: 100, width: 100, height: 30)
+        lblEmail.text = "이메일"
+        
+        // 3. 레이블의 폰트를 설정한다.
+        lblEmail.font = UIFont.systemFont(ofSize: 14)
+        
+        // 4. 레이블을 루트뷰에 추가한다.
+        self.view.addSubview(lblEmail)
+        
+        // 위와 마찬가지고 자동갱신 레이블을 생성하고 루트뷰에 추가한다.
+        let lblUpdate = UILabel()
+        lblUpdate.frame = CGRect(x: 30, y: 150, width: 100, height: 30)
+        lblUpdate.text = "자동갱신"
+        lblUpdate.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblUpdate)
+        
+        // 갱신주기 레이블을 생성하고 루트뷰에 추가한다
+        let lblInterval = UILabel()
+        lblInterval.frame = CGRect(x: 30, y: 200, width: 100, height: 30)
+        lblInterval.text = "갱신주기"
+        lblInterval.font = UIFont.systemFont(ofSize: 14)
+        
+        self.view.addSubview(lblInterval)
     }
 
 
