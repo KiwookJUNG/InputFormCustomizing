7d1e22890e194bac8f99c2f5b74c97ef9973fb7e
정기욱 <rldnr56@me.com>
2019-02-14 17:51:20 +0900

    프로그래밍 방식을 사용하여 레이블 옆에 텍스트필드(이메일), 스위치, 스테퍼 등을 구현하였고 두개의 메소드를 사용해 스위치와 스테퍼의 액션 메소드를 추가해줌.

--- InputFormCustomizing/ViewController.swift
+++ InputFormCustomizing/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var paramEmail: UITextField! // 텍스트 필드
+    var paramUpdate: UISwitch! // 스위치
+    var paramInterval: UIStepper! // 스테퍼
+    var txtUpdate: UILabel! // 스위치 컨트롤의 값을 표현할 레이블
+    var txtInterval: UILabel! // 스테퍼 컨트롤의 값을 표현할 레이블
+    
     override func viewDidLoad() {
         // 1. 네비게이션 바 타이틀을 입력한다.
         self.navigationItem.title = "설정"
@@ -40,8 +46,65 @@ class ViewController: UIViewController {
         lblInterval.font = UIFont.systemFont(ofSize: 14)
         
         self.view.addSubview(lblInterval)
+        
+        self.paramEmail = UITextField()
+        self.paramEmail.frame = CGRect(x: 120, y: 100, width: 220, height: 30)
+        self.paramEmail.font = UIFont.systemFont(ofSize: 13)
+        self.paramEmail.borderStyle = .roundedRect
+        self.paramEmail.autocapitalizationType = .none
+        
+        self.view.addSubview(self.paramEmail)
+        
+        // 스위치 객체를 생성한 후 위치 설정
+        self.paramUpdate = UISwitch()
+        self.paramUpdate.frame = CGRect(x: 120, y: 150, width: 50, height: 30)
+        
+        // 스위치가 On 되어 있는 상태를 기본값으로 설정한다.
+        self.paramUpdate.setOn(true, animated: true)
+        
+        self.view.addSubview(self.paramUpdate)
+        
+        
+        // 갱신주기를 위한 스테퍼를 추가한다.
+        self.paramInterval = UIStepper()
+        
+        self.paramInterval.frame = CGRect(x: 120, y: 200, width: 50, height: 30)
+        self.paramInterval.minimumValue = 0 // 1. 스테퍼가 가질 수 있는 최소값
+        self.paramInterval.maximumValue = 100 // 2. 스테퍼가 가질 수 있는 최대값
+        self.paramInterval.stepValue = 1 // 3. 스테퍼의 값 변경 단위
+        self.paramInterval.value = 0 // 4. 초기값 설정
+        
+        self.view.addSubview(self.paramInterval)
+        
+        // 스위치 객체의 값을 표현할 레이블을 추가한다.
+        self.txtUpdate = UILabel()
+        
+        self.txtUpdate.frame = CGRect(x: 250, y: 150, width: 100, height: 30)
+        self.txtUpdate.font = UIFont.systemFont(ofSize: 12)
+        self.txtUpdate.textColor = UIColor.red // 텍스트 색상 설정
+        self.txtUpdate.text = "갱신함" // 2. " 갱신함 " 또는 " 갱신하지 않음 "
+        
+        self.view.addSubview(self.txtUpdate)
+        
+        // 스테퍼의 값을 텍스트로 표현할 레이블을 추가한다.
+        self.txtInterval = UILabel()
+        self.txtInterval.frame = CGRect(x: 250, y: 200, width: 100, height: 30)
+        self.txtInterval.font = UIFont.systemFont(ofSize: 12)
+        self.txtInterval.textColor = UIColor.red
+        self.txtInterval.text = "0분마다"
+        
+        self.view.addSubview(self.txtInterval)
+        
+        self.paramUpdate.addTarget(self, action: #selector(presentUpdateValue(_:)), for: .valueChanged)
+        self.paramInterval.addTarget(self, action: #selector(presentIntervalValue(_:)), for: .valueChanged)
     }
 
-
+    // 스위치와 상호반을할 액션 메소드
+    @objc func presentUpdateValue(_ sender: UISwitch) {
+        self.txtUpdate.text = (sender.isOn == true ? "갱신함" : "갱신하지 않음")
+    }
+    @objc func presentIntervalValue(_ sender: UIStepper) {
+        self.txtInterval.text = ("\( Int(sender.value) )분마다")
+    }
 }
 
